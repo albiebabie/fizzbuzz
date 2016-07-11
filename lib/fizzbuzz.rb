a5bbd5423b8c8b7bf780d1cf219c2e59066d4f77
albiebabie <albiebabie@gmail.com>
2016-07-11 08:10:33 +0100

    passed test 7

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,8 @@
+def fizzbuzz(number)
+  return 'fizzbuzz' if number % 15 == 0
+  return 'fizz' if number == 3
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number == 5
+  return 'buzz' if number % 5 == 0
+  return number
+end
