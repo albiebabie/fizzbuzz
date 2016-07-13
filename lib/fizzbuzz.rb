371b8d83dec92e6808b14a9ac91bd2ada7a705c5
albiebabie <albiebabie@gmail.com>
2016-07-13 09:25:07 +0100

    passed test 8

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  return 'fizz' if number == 3
+  return 'buzz' if number == 5
+  return 'fizzbuzz' if number == 15
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number % 5 == 0
+  return 'fizzbuzz' if number % 15 == 0
+  number
+end
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,25 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizz" when passed number divisible by 3' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  it 'returns "buzz" when number divisible by 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed number divisible by 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "number" if number doesn\'t fizz buzz or fizzbuzz' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
