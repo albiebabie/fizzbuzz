15f4267b2810f619af108fdfbb28fb5a070b2e4c
albiebabie <albiebabie@gmail.com>
2016-07-13 09:02:25 +0100

    clean slate 7

--- lib/fizzbuzz.rb
+++
@@ -1,8 +0,0 @@
-def fizzbuzz(number)
-  return 'fizzbuzz' if number % 15 == 0
-  return 'fizz' if number == 3
-  return 'fizz' if number % 3 == 0
-  return 'buzz' if number == 5
-  return 'buzz' if number % 5 == 0
-  return number
-end
